7d8f63f32bad653e7676ce101e6fe511004fb1ef
Joseph Chitsungo <50632784+carles2019@users.noreply.github.com>
2021-05-10 01:39:13 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+define ("GREETING", "Hello, I will calculate area of a rectangle", true );
+echo GREETING;
+
+
+echo "-----------------------------------------------------------------------------";
+
+
+function rect_area($length = 2, $width = 4) //function has declared
+{
+    $area = $length * $width;
+    echo "Area Of Rectangle with length " . $length . " & width " . $width . " is " . $area ;
+}
+rect_area(); // function has been called.
+
+
+?>
